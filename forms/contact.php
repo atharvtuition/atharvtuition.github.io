2a5431494b13e5425f45d957c9441b25ff105716
Kannan G R <kannanravi2105@gmail.com>
2021-07-09 21:47:24 +0530

    Changes in php structure

--- forms/contact.php
+++ forms/contact.php
@@ -1,7 +1,7 @@
 <?php
 
 // Define some constants
-define( "RECIPIENT_NAME", "Contact" );
+define( "RECIPIENT_NAME", "Atharv Tuition Center" );
 define( "RECIPIENT_EMAIL", "atharvtuition.education@gmail.com" );
 
 
@@ -9,23 +9,22 @@ define( "RECIPIENT_EMAIL", "atharvtuition.education@gmail.com" );
 $success = false;
 $sendername = isset( $_POST['name'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['name'] ) : "";
 $senderEmail = isset( $_POST['email'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['email'] ) : "";
-$senderrequirements = isset( $_POST['requirements'] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST['requirements'] ) : "";
 $message = isset( $_POST['message'] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST['message'] ) : "";
 
 // If all values exist, send the email
 if ( $sendername && $senderEmail && $message) {
   $recipient = RECIPIENT_NAME . " <" . RECIPIENT_EMAIL . ">";
   $headers = " From: " . $senderEmail ;
-  $msgBody = "Requirements: ".$senderrequirements."\n".$message;
+  $msgBody = "\n".$message;
   $success = mail( $recipient, $headers, $msgBody );
 
   //Set Location After Successsfull Submission
-  header('Location: index.html?message=Successfull');
+  header('Location: ../index.html');
 }
 
 else{
 	//Set Location After Unsuccesssfull Submission
-  	header('Location: index.html?message=Failed');	
+  	header('Location: ../index.html');	
 }
 
 ?>
